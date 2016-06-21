2658f98b0a3dc9ede03b1ded185a1b0ea0d33ed9
Pavel Martynov <martynov.pl@gmail.com>
2016-06-20 21:41:11 -0700

    Implemented simple fetch client

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import {Component} from '@angular/core';
 
+declare function fetch(url);
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><pre>{{ json }}</pre>'
 })
-export class AppComponent { }
+export class AppComponent {
+    public json: string;
+
+    public ngOnInit() {
+        fetch("test.json")
+            .then(response => response.json())
+            .then(json => this.json = JSON.stringify(json));
+    }
+}
